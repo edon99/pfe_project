445816541a498bd5201b072b97da4e1c79bcf3ae
edon99 <ofoura07@gmail.com>
2024-06-16 00:03:50 +0100

    player detection with color and team assignment

--- pages/player_detection.py
+++ pages/player_detection.py
@@ -7,18 +7,23 @@ import cv2
 import skimage
 from PIL import Image, ImageColor
 from ultralytics import YOLO
-from detect import detect
+from detect import detect, detect_test
 
 
-model = YOLO("models/players.pt")
 
+model = YOLO("models/best.pt")
 
     ##INTERFACE
 st.write("# Player Detection")    
+
 video_path=st.file_uploader("Upload your video")  
 if video_path is None:
     st.write("Please import your video")
 else:
+    team1 = st.text_input(label="Team 1",value="Team A")
+    team2 = st.text_input(label="Team 2", value="Team B")
+
+
     start_button = st.button("Start detection","start")
     stop_button = st.button("Stop detection","stop")
 
@@ -31,8 +36,8 @@ else:
 
     if start_button and not stop_button:
         st.toast(f'Detection Started!')
-        detect(stframe, cap, model)
-    else:
+        detect_test(stframe, cap, model, team1, team2)
+    elif stop_button:
         st.toast(f'Detection Stopped!')
         cap.release()
             
