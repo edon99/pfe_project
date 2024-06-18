fdfa561a5ff25a30b66ae4e8b4a92ecedc0b0aff
hacenbarb <cenha06@gmail.com>
2024-06-18 11:19:02 +0100

    fix issues in tactical_map

--- pages/tactical_map.py
+++ pages/tactical_map.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import tempfile
-import numpy as np
 import cv2
 from utils.load_model import load_models
 from utils.detection import detect
@@ -18,44 +17,47 @@ def main():
     st.write("# 2D Tactical Map")
     input_vide_file = st.file_uploader(
         'Upload your match', type=['mp4', 'mov', 'avi', 'm4v', 'asf'])
-    save_output = st.checkbox(label='Save output', value=False)
-    if save_output:
-        output_file_name = st.text_input(
-            label='File Name (Optional)',
-            placeholder='Enter output video file name.')
+    if input_vide_file is None:
+        st.write("Please import your video")
     else:
-        output_file_name = 'out'
-    if input_vide_file is not None:
+        save_output = st.checkbox(label='Save output', value=False)
+        if save_output:
+            output_file_name = st.text_input(
+                label='File Name (Optional)',
+                placeholder='Enter output video file name.')
+        else:
+            output_file_name = 'out'
         col1, col2 = st.columns(2)
         with col1:
             start_button = st.button("Start detection", "start")
         with col2:
             stop_button = st.button("Stop detection", "stop")
 
-    tempf = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False)
-    tempf.write(input_vide_file.read())
-    stframe = st.empty()
-    cap = cv2.VideoCapture(tempf.name)
-    status = False
-    if start_button and not stop_button:
-        st.toast(f'Detection Started!')
-        status = detect(cap,
-                        stframe,
-                        output_file_name,
-                        save_output,
-                        players_model,
-                        keypoints_model,
-                        tac_map = tac_map,
-                        num_pal_colors=3)
-        cap.release()
-    else:
-        try:
+        tempf = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False)
+        tempf.write(input_vide_file.read())
+        stframe = st.empty()
+        cap = cv2.VideoCapture(tempf.name)
+        status = False
+
+        if start_button and not stop_button:
+            st.toast(f'Detection Started!')
+            status = detect(cap,
+                            stframe,
+                            output_file_name,
+                            save_output,
+                            players_model,
+                            keypoints_model,
+                            tac_map = tac_map,
+                            num_pal_colors=3)
             cap.release()
-        except:
-            pass
-    if stop_button or status:
-        st.toast(f'Detection Stopped!')
-        cap.release()
+        else:
+            try:
+                cap.release()
+            except:
+                pass
+        if stop_button or status:
+            st.toast(f'Detection Stopped!')
+            cap.release()
 
 
 if __name__ == '__main__':
