b3fc4befc492a5adc71ef8b456f53eaab3c72712
edon99 <ofoura07@gmail.com>
2024-06-23 21:10:31 +0100

    statc chghol cleanup

--- src/pages/stats.py
+++ src/pages/stats.py
@@ -18,20 +18,6 @@ def load_data(team_choice):
 teams=list(matches['home_team_name'].drop_duplicates())
 
 
-
-st.markdown(
-    """
-    <style>
-        .sidebar .sidebar-content {
-            font-size: 20px;
-            color: blue;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
 teams_choice = st.sidebar.selectbox('Team', teams)
 
 
@@ -42,17 +28,10 @@ if teams_choice:
     header_color = '#F5F5DC'  
 
 
-
-
-    st.markdown('<h1 class="custom-title">Match Information </h1>', unsafe_allow_html=True)
-
-    text = "This table presents details of the matches played by the selected team in the 2023 Africa Cup of Nations."
-
-# Utilisez le balisage HTML et CSS pour définir la couleur beige
-    beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
+    text = "This table presents details of the matches played by the selected team"
 
 # Affichez le texte stylisé dans Streamlit
-    st.markdown(beige_text, unsafe_allow_html=True)
+    st.markdown(text, unsafe_allow_html=True)
     st.write(game)    
 
 else: 
@@ -94,25 +73,20 @@ if match_choice:
 
 
     if selected_plot == 'Passing Network':
-       #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
        st.markdown('<h1 class="custom-subheader">Passing Network: </h1>', unsafe_allow_html=True)
        text="A [passing network](https://statsbomb.com/articles/soccer/explaining-xgchain-passing-networks/) is  the application of network theory and social network analysis to passing data in football. Each player is a node, and the passes between them are connections."
-       beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-       st.markdown(beige_text, unsafe_allow_html=True)
+       st.markdown(text, unsafe_allow_html=True)
        passnetwork(match_id,selected_team)
        \
   
 
     if selected_plot == 'Passes':
-       #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
        st.markdown('<h1 class="custom-subheader">Ball Pass: </h1>', unsafe_allow_html=True)
        text="Ball is passed between teammates."
-       beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-       st.markdown(beige_text, unsafe_allow_html=True)
+       st.markdown(text, unsafe_allow_html=True)
        st.markdown('<h1 class="custom-subheader">Ball Receipt: </h1>', unsafe_allow_html=True)
        text="The receipt or intended receipt of a pass."
-       beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-       st.markdown(beige_text, unsafe_allow_html=True)
+       st.markdown(text, unsafe_allow_html=True)
        passe(df,selected_team)
 
        
@@ -121,16 +95,14 @@ if match_choice:
          #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Pressure: </h1>', unsafe_allow_html=True)
          text="Applying pressure to an opposing player who’s receiving, carrying or releasing the ball."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          pressure_heatmap(df,selected_team)
 
     elif selected_plot == 'Pressure heat map Juego de Posición':
          #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Pressure Juego de Posición: </h1>', unsafe_allow_html=True)
          text="Percentage of pressure applied by the selected team according to [Juego de Posición](https://breakingthelines.com/tactical-analysis/what-is-juego-de-posicion/)."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
 
          persure_juego(df,selected_team) 
 
@@ -140,23 +112,19 @@ if match_choice:
          #st.markdown('<h1 class="custom-subheader">Assists: </h1>', unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Shot assist: </h1>', unsafe_allow_html=True)
          text="The pass was an assist to a shot (that did not score a goal)."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Goal assist: </h1>', unsafe_allow_html=True)
          text="The pass was an assist to a goal."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
 
          assists(df_team_selected,selected_team)
          \
 
 
     elif selected_plot == 'Shots':
-          #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
           st.markdown('<h1 class="custom-subheader">Shot: </h1>', unsafe_allow_html=True)
           text="An attempt to score a goal, made with any (legal) part of the body."
-          beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-          st.markdown(beige_text, unsafe_allow_html=True)
+          st.markdown(text, unsafe_allow_html=True)
 
           shot(df_team_selected)
 
@@ -166,62 +134,47 @@ if match_choice:
          #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Forward pass: </h1>', unsafe_allow_html=True)
          text='All passes into the final third of the pitch.'
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          transition(df_team_selected)
   
     elif selected_plot == 'Crosses':
-         #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Cross: </h1>', unsafe_allow_html=True)
-
          text='A [cross](https://www.soccerhelp.com/terms/soccer-cross.shtml) is a "square pass" to the area in front of the goal.'
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          pass_cross(df_team_selected)
         
     elif selected_plot == 'Mistakes':
-          #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
           st.markdown('<h1 class="custom-subheader">Dispossessed: </h1>', unsafe_allow_html=True)
           text="Player loses ball to an opponent as a result of being tackled by a defender without attempting a dribble."
-          beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-          st.markdown(beige_text, unsafe_allow_html=True)
+          st.markdown(text, unsafe_allow_html=True)
           st.markdown('<h1 class="custom-subheader">Miscontrol: </h1>', unsafe_allow_html=True)
           text="Player loses ball due to bad touch."
-          beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-          st.markdown(beige_text, unsafe_allow_html=True)
+          st.markdown(text, unsafe_allow_html=True)
           st.markdown('<h1 class="custom-subheader">Foul Committed: </h1>', unsafe_allow_html=True)
           text="Any infringement that is penalised as foul play by a referee. Offside are not tagged as afoul committed.."
-          beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-          st.markdown(beige_text, unsafe_allow_html=True)
+          st.markdown(text, unsafe_allow_html=True)
           st.markdown('<h1 class="custom-subheader">Error: </h1>', unsafe_allow_html=True)
           text="When a player is judged to make an on-the-ball mistake that leads to a shot on goal."
-          beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-          st.markdown(beige_text, unsafe_allow_html=True)
+          st.markdown(text, unsafe_allow_html=True)
           mistake(df_team_selected)
 
 
     
 
     elif selected_plot == 'Defensive actions':
-         #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
-
          st.markdown('<h1 class="custom-subheader">Clearance: </h1>', unsafe_allow_html=True)
          text="Action by a defending player to clear the danger without an intention to deliver it to a teammate."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Block: </h1>', unsafe_allow_html=True)
          text="Blocking the ball by standing in its path."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
 
          st.markdown('<h1 class="custom-subheader">Interception: </h1>', unsafe_allow_html=True)
          text="Preventing an opponent's pass from reaching their teammates by moving to the passing lane/reacting to intercept it."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Ball Recovery: </h1>', unsafe_allow_html=True)
          text="An attempt to recover a loose ball."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          defensive_actions(df_team_selected)
          \
          \
@@ -231,31 +184,25 @@ if match_choice:
          \
      
     elif selected_plot == 'Player performance':
-         #st.markdown('<h1 class="custom-header">Description </h1>', unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Pass: </h1>', unsafe_allow_html=True)
          text="The Ball is passed by the player selected."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Carry: </h1>', unsafe_allow_html=True)
          text="The player controls the ball at their feet while moving or standing still."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
 
          st.markdown('<h1 class="custom-subheader">Under pressure: </h1>', unsafe_allow_html=True)
          text='The action was performed while being pressured by an opponent.'
   
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          st.markdown('<h1 class="custom-subheader">Counterpress: </h1>', unsafe_allow_html=True)
          text="Pressing actions within 5 seconds of an open play turnover."
-         beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-         st.markdown(beige_text, unsafe_allow_html=True)
+         st.markdown(text, unsafe_allow_html=True)
          player(df_team_selected)
 
 else:
-    text="Please select a match from 'Match' and the event type you would like to display on the screen from 'VizZ type' dropdown menu on the left."
-    beige_text = f'<span style="color: #F5F5DC;font-size: 20px">{text}</span>'
-    st.markdown(beige_text, unsafe_allow_html=True)
+    text="Please select a match from 'Match' and the event type you would like to display"
+    st.markdown(text, unsafe_allow_html=True)
 
 
    
